07d371f664f05a90853cc09f0d1d01a05e6e5d71
徐臻杰 <1929509811@qq.com>
2024-04-10 21:28:37 +0800

    🧨Added game world background processing.

---
+++ flutter_dash_doodle_jump/lib/world.dart
@@ -0,0 +1,21 @@
+import 'package:flame/components.dart';
+import 'package:flame/parallax.dart';
+import 'package:flutter/cupertino.dart';
+
+import 'flutter_dash_doodle_jump.dart';
+
+class World extends ParallaxComponent<FlutterDashDoodleJump> {
+  @override
+  Future<void> onLoad() async {
+    parallax = await gameRef.loadParallax(
+      [
+        ParallaxImageData('game/background/background.png'),
+      ],
+      fill: LayerFill.width,
+      repeat: ImageRepeat.repeat,
+      //时差效果
+      // baseVelocity: Vector2(0, -5),
+      // velocityMultiplierDelta: Vector2(0, 1.2),
+    );
+  }
+}
